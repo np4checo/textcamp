4fdf0a0ece6136ac24b46dc50befaf1a48827283
Natã Pacheco <natan.pacheco.1998@gmail.com>
2022-10-13 23:47:34 -0300

    primeiro-commit

---
+++ src/components/TextCamp.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useRef, useMemo } from 'react';
+import JoditEditor from 'jodit-react';
+import './TextCamp.css';
+
+const TextCamp = ({}) => {
+	const editor = useRef(null);
+	const [content, setContent] = useState('');
+
+	const config =
+		{
+			readonly: false,
+			height: 400,
+		};
+		const handleSave = () => {
+			localStorage.setItem("document", content);
+		};
+		const handleLoad = () => {
+			setContent(localStorage.getItem("document"))
+		};
+
+	return (
+		<>
+			<JoditEditor
+			ref={editor}
+			value={content}
+			config={config}
+			tabIndex={1} 
+			onBlur={(newContent) => setContent(newContent)}
+			/>
+		</>
+	);
+};
+
+export default TextCamp;
